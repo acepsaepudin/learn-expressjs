40921c830eeabe0296e0c1a44143b26681a8dac2
Acep Saepudin <qwertynesia@gmail.com>
2021-01-23 23:49:41 +0700

    try basic routing

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ const app = express()
 const port = 3333
 
 app.get('/', (req, res) => {
-  res.send('hohoho')
+  res.status(404)
+  res.send({
+    error: 'error again'
+  })
+})
+
+// our custom JSON 404 middleware. Since it's placed last
+// it will be the last middleware called, if all others
+// invoke next() and do not respond.
+app.use(function(req, res){
+  res.status(404);
+  res.send({ error: "Content not found." });
+});
+
+app.post('/', function(req, res) {
+  res.send('post method!')
 })
 
 
